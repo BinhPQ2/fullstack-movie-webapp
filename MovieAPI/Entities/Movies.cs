af213b9300c45b28ac6dc2254a17b8c4f78c0d42
azul <binh.pham@dklab.usgit config  user.email binh.pham@dklab.us>
2025-09-16 05:42:59 +0700

    Initial commit.

---
+++ MovieAPI/Entities/Movies.cs
@@ -0,0 +1,11 @@
+namespace MovieAPI.Entities
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+    }
+}
